ffeda0066a24a42c7eb3993d067992ce71b015b4
Gaurav Kumar(Technology-New Delhi) <gaurav.kumar@htdigital.in>
2024-07-02 23:15:56 +0530

    Hello

--- hello.java
+++ hello.java
@@ -3,7 +3,5 @@ package Test;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello!");
-        System.out.println("I am in Test 1 file");
-        System.out.println("Doing changes in test1");
     }
 }
