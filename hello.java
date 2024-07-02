5559030c9c39c3b0d027f86f4702a8d7f9e6ee43
Gaurav Kumar(Technology-New Delhi) <gaurav.kumar@htdigital.in>
2024-07-02 18:57:01 +0530

    First commit

--- hello.java
+++ hello.java
@@ -3,10 +3,5 @@ package Test;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello!");
-        System.out.println("Slight change done!");
-        System.out.println("Third commit it is:");
-        System.out.println("This is 4th commit!");
-        System.out.println("This is 5th commit");
-        System.out.println("In main branch now!");
     }
 }
